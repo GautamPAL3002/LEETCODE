ff136fcbb3fc579201183e762129e1690de48b9e
Gautam pal <gautampal1208@gmail.com>
2025-05-09 06:48:36 +0530

    Time: 0 ms (100%), Space: 55.9 MB (88.7%) - LeetHub

--- 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.java
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.java
@@ -3,6 +3,12 @@ class Solution {
         int start=0;
         int end=arr.length-1;
         while(start<end){
+            // add.trick to handle dublicant edge case;
+            //dublicant on left side
+            while(start<end&&arr[start]==arr[start+1])
+            start++;
+            while(start<end&&arr[end]==arr[end-1])
+            end--;
             int mid=start+(end-start)/2;
             if(arr[mid]>arr[mid+1]){
             end=mid;}
